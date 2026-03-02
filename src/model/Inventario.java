52a0d777aff8f0f8a0dbda48045cbe4f45cfb606
dealbaliam432-rgb <dealbaliam432@gmail.com>
2026-03-02 16:43:20 -0500

    Update Inventario.java

--- src/model/Inventario.java
+++ src/model/Inventario.java
@@ -2,20 +2,20 @@ package model;
 import java.util.ArrayList;
 
 public class Inventario {
-    // Requisito: Estructura de datos (ArrayList)
+    // es un Requisito lo cual es la  Estructura de datos es decir el ArrayList
     private ArrayList<Producto> listaProductos;
 
     public Inventario() {
         this.listaProductos = new ArrayList<>();
     }
 
-    // Método funcional para agregar productos
+    // sirve  para agregar productos
     public void agregarProducto(Producto p) {
         listaProductos.add(p);
         System.out.println("Sistema: " + p.getNombre() + " agregado al inventario.");
     }
 
-    // Método para buscar (Eficiencia)
+    // sirve para ser eficientes
     public Producto buscarPorCodigo(String codigo) {
         for (Producto p : listaProductos) {
             if (p.getCodigo().equals(codigo)) {
